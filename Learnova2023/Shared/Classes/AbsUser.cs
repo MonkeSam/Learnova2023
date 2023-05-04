6d60e72cfcac4b1de10a61639d9d347fad271838
MonkeSam <100468218+MonkeSam@users.noreply.github.com>
2023-05-04 21:50:42 +0200

    Pagina Login iniziata

--- Learnova2023/Shared/Classes/AbsUser.cs
+++ Learnova2023/Shared/Classes/AbsUser.cs
@@ -8,6 +8,14 @@
         public string? Cognome { get; set; }
         public string? User { get; set; }
 
+        public AbsUser(string n, string c)
+        {
+            this.Id = 0;
+            this.Nome = n;
+            this.Cognome = c;
+            this.User = null;
+        }
+
         public AbsUser(int id,string n,string c,string u)
         {
             this.Id = id;
--- Learnova2023/Shared/Classes/Professore.cs
+++ Learnova2023/Shared/Classes/Professore.cs
@@ -4,6 +4,11 @@
     {
         public int Stipendio { get; set; }
 
+        public Professore(string n, string c):base(n,c)
+        {
+            this.Stipendio = 0;
+        }
+
         public Professore(int id,string nome,string cognome,string user,int stipendio):base(id,nome,cognome,user)
         {
             this.Stipendio= stipendio;
