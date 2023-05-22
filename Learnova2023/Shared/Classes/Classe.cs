01d19dc7dc98db11f195cf203c354feea584bd5a
cris <crist@CRISTIAN>
2023-05-22 11:46:51 +0200

    Pagina Elenco Studenti fatta e tasto note Fix Generali: Pagina Elenco classi (unito sezione ad anno e sortato direttamente), messo nel dialog per i compiti una select con i soli 2 tipi disponibili (Verifica, Interrogazione)

--- Learnova2023/Shared/Classes/Classe.cs
+++ Learnova2023/Shared/Classes/Classe.cs
@@ -15,6 +15,10 @@
             this.Sezione = sezione;
             this.Indirizzo= indirizzo;
         }
+        public string RitornaClasse()
+        {
+            return $"{Anno} {Sezione}";
+        }
         public override string ToString()
         {
             return $"{Anno} {Sezione} {Indirizzo}" ;
