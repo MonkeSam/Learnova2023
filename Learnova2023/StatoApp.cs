4dc256651c07730285ff16c0128d75c0452b3fda
samuele.trapani <samuele.trapani@PC7322>
2023-05-31 13:54:54 +0200

    Passaggio idclasse fixato TODO:Dialog gestione studenti

--- Learnova2023/StatoApp.cs
+++ Learnova2023/StatoApp.cs
@@ -8,12 +8,13 @@
 
         public dynamic Details { get; set; }
 
+        public int  idClasse { get; set; }
+
         public void SetSession(string user, string ss, dynamic d)
         {
             this.Username = user;
             this.SessionString = ss;
             this.Details = d;
-
         }
     }
 }
