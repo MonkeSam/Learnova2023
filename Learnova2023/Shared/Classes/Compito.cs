eab67332347b5649983bfaae23428311d3e55a6a
MonkeSam <100468218+MonkeSam@users.noreply.github.com>
2023-06-11 00:15:03 +0200

    dialog compiti a prova di analfabeta digitale

--- Learnova2023/Shared/Classes/Compito.cs
+++ Learnova2023/Shared/Classes/Compito.cs
@@ -1,13 +1,18 @@
-﻿namespace Learnova2023.Shared.Classes
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Learnova2023.Shared.Classes
 {
     public class Compito
     {
         public int Id { get; set; }
-        public DateTime? Date { get; set; }
-        public string? Tipo { get; set; }
+
+        [Required] public DateTime? Date { get; set; }
+
+        [Required] public string? Tipo { get; set; }
+
         public Professore? Professore { get; set; }
-        public Materia? Materia { get; set; }
-        public string? Descrizione { get; set; }
+        [Required] public Materia? Materia { get; set; }
+        [Required] public string? Descrizione { get; set; }
         public Compito() { }
         public Compito(DateTime d, string t, Professore p, Materia m, string des, int id)
         {
@@ -21,14 +26,18 @@
         }
         public bool Equals(Compito c)
         {
+
+
             if (this.Date == c.Date && this.Materia == c.Materia && this.Tipo == c.Tipo && this.Descrizione == c.Descrizione)
             {
                 return true;
             }
-            else { return false; }
 
+
+
+            return false;
         }
-       
+
 
     }
 }
